04abb6efe9c893f4112bc09c2290043972fe9b4d
kiwimchen <kiwimchen@tencent.com>
2022-11-19 14:49:41 +0800

    test bin signer

--- assistd.rb
+++ assistd.rb
@@ -1,8 +1,8 @@
 class Assistd < Formula
   desc "Assistd CLI for the Udt platform API"
   homepage "https://udt.woa.com/"
-  url "https://github.com/assistd/homebrew-tap/releases/download/v1.0.0/assistd-v1.0.0_Darwin_x86_64.tar.gz"
-  sha256 "934c7199d483e0ee23295663abbab99b5a48b1a79b9e3a82d41518b7bd81a529"
+  url "https://github.com/HEZI0427/udt/releases/download/v1.0.0/assistd-v1.0.0_Darwin_x86_64.tar.gz"
+  sha256 "115429e76fe986f17e2fca0d44122528c6e1c82a952490b260f4c0c8f60c969f"
   license ""
 
   def install
